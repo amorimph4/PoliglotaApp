0105e3749559e1e3f56ceaf5a37a6e73d325baa6
Pedro Amorim <pedro.henrique@bsy.com.br>
2019-10-13 23:37:06 -0300

    adicionando etapas na criação dos usuários

--- src/app/auth/register/register.page.ts
+++ src/app/auth/register/register.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from  "@angular/router";
 import { AuthService } from '../auth.service';
+import { UsertypeService } from '../../services/usertype.service';
 import { Storage } from  '@ionic/storage';
 
 @Component({
@@ -10,15 +11,19 @@ import { Storage } from  '@ionic/storage';
 })
 export class RegisterPage implements OnInit {
 
-  constructor(private  authService:  AuthService, private  router:  Router, private  storage:  Storage) { }
+  constructor(private  authService:  AuthService, private  router:  Router, private  storage:  Storage, private userService: UsertypeService) { }
 
+  etapa: number = 2;
   type : string;
 
   ngOnInit() {
     this.storage.get('UserType').then((val) => {
-      console.log('Your age is', val);
       this.type = val;
     });
+  }
+
+  setEtapa(etapa) {
+    this.etapa = etapa;
   }
 
   register(form) {
